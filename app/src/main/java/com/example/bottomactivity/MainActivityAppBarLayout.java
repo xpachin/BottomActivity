62f699bc85e69bd47a4d57dec0162828ed9ba1e2
XP <xpachin@gmail.com>
2021-05-07 02:49:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bottomactivity/MainActivityAppBarLayout.java
@@ -0,0 +1,14 @@
+package com.example.bottomactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityAppBarLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app_bar_layout);
+    }
+}
